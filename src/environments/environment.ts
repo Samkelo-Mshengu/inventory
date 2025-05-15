7557e8b478ca4da2944475e208da0a7c268e410f
Samkelo-Mshengu <mshengusamkelo337@gmail.com>
2025-05-15 23:20:22 +0200

    First commit fixed environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
  
   firebaseConfig:{
-    apiKey: "AIzaSyC_3JLefO6lge5b13WF2n3AKPHem31QVGI",
-    authDomain: "inventory-37c2b.firebaseapp.com",
-    projectId: "inventory-37c2b",
-    storageBucket: "inventory-37c2b.appspot.com",
-    messagingSenderId: "352083357567",
-    appId: "1:352083357567:web:aab4344c766d5e19f4b44a",
-    measurementId: "G-4Z8LYCGQKJ"
+   apiKey: "AIzaSyA2G8ukX1f--HRWzT2_aCi-pPS2t5mSWXU",
+  authDomain: "bestbrightness-8c9c6.firebaseapp.com",
+  projectId: "bestbrightness-8c9c6",
+  storageBucket: "bestbrightness-8c9c6.appspot.com",
+  messagingSenderId: "31475353699",
+  appId: "1:31475353699:web:0d7c29b57be37e6f061056",
+  measurementId: "G-BV877LSEQK"
   }
 };
 
